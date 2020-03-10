6860b6ae7267da793713431a99099734512a4ea7
TMJ97 <s185027@student.dtu.dk>
2020-03-10 11:46:46 +0100

    binary crap please go away

--- src/main/java/galgeleg/GalgelegServer.java
+++ src/main/java/galgeleg/GalgelegServer.java
@@ -4,7 +4,7 @@ import java.rmi.Naming;
 
 public class GalgelegServer {
     public static void main(String[] arg) throws Exception {
-//        System.setProperty("java.rmi.server.hostname", "ec2-user@ec2-3-21-41-28.us-east-2.compute.amazonaws.com");
+        System.setProperty("java.rmi.server.hostname", "ec2-3-21-41-28.us-east-2.compute.amazonaws.com");
         java.rmi.registry.LocateRegistry.createRegistry(9927);
         GalgelegI spil = new GalgelegImpl();
 
